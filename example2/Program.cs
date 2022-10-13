3edfa46b344821e4c35c8006dd560aa074bfd1f9
VladimirSharldaev <vladimirsarldaev43246@gmail.com>
2022-10-13 23:30:12 +0300

    add example2

---
+++ example2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+Console.WriteLine("Введите три числа через enter");
+
+int count = 0;
+int a = int.Parse(Console.ReadLine()!);
+int max = a;
+int min = a;
+while(count<2) //можно задать любое количество чисел для сравнения
+{int b = int.Parse(Console.ReadLine()!);
+ if (b>max) max = b;
+ if (b<min) min = b;
+ count++;
+ }
+
+Console.WriteLine("max="+max);
+Console.WriteLine("min="+min);
+
+//наверное можно было сделать легче, но что ниего в голову не пришло.
